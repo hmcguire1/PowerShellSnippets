6c2eefcda2bf1ce70bd4882739fc7eb0c7d7d771
Hunter McGuire <hmcguire8621@gmail.com>
2019-02-09 08:01:09 -0500

    resource group inventory commit

---
+++ resource_group_inventory.ps1
@@ -0,0 +1,27 @@
+$ResourceGroup = Read-Host "Please Enter Resource Group" 
+
+$VMList = Get-AzVM -ResourceGroupName $ResourceGroup -Verbose
+$NetworkResources = Get-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -Verbose
+
+$VMs = foreach ($VM in $VMList) {
+
+    $NetIf = ($NetworkResources | Where-Object {$_.Parameters.virtualMachineName.Value -eq $VM.Name} | Select-Object -First 1).Parameters.publicIpAddressName.Value
+    $NSG = ($NetworkResources | Where-Object {$_.Parameters.virtualMachineName.Value -eq $VM.Name} | Select-Object -First 1).Parameters.networkSecurityGroupId.Value
+    $Image = $VM.StorageProfile.ImageReference.Id
+
+    [PSCustomObject]@{
+
+        VMName        = $VM.Name
+        Region        = $VM.Location
+        VmSize        = $VM.HardwareProfile.VmSize
+        VmHostName    = $VM.OSProfile.ComputerName
+        ResourceGroup = $VM.ResourceGroupName
+        Image         = $(if ($Image) {$Image.Substring($Image.LastIndexOf('/') + 1)})
+        Status        = $VM.StatusCode
+        DeployState   = $VM.ProvisioningState
+        PublicIP      = (Get-AzPublicIpAddress -ResourceGroupName $ResourceGroup -Name $NetIf | Select-Object IpAddress -Verbose).IpAddress
+        NSG           = $(if ($NSG) {$NSG.Substring($NSG.LastIndexOf('/') + 1)})
+    }    
+}
+
+$VMs | Sort-Object -Descending | Format-Table
