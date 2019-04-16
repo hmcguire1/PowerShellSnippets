95e23d7c5fffee7df7ad9acfcc4453d10b421085
hmcguire1 <hmcguire8621@gmail.com>
2019-04-16 10:39:25 -0400

    Changed input to parameter from command line

--- resource_group_inventory.ps1
+++ resource_group_inventory.ps1
@@ -1,28 +1,42 @@
-$ResourceGroup = Read-Host "Please Enter Resource Group" 
+[CmdletBinding()]
+Param(                      
+    [String]$ResourceGroup,
+    [Switch]$json
+)
 
-$VMList = Get-AzVM -ResourceGroupName $ResourceGroup
-$NetworkResources = Get-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup
-$PublicIps = Get-AzPublicIpAddress -ResourceGroupName $ResourceGroup | select Name, IpAddress
+if ($ResourceGroup) {
 
-$VMs = foreach ($VM in $VMList) {
+    $VMList = Get-AzVM -ResourceGroupName $ResourceGroup
+    $NetworkResources = Get-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup
+    $PublicIps = Get-AzPublicIpAddress -ResourceGroupName $ResourceGroup | select Name, IpAddress
 
-    $NetIp = ($NetworkResources | Where-Object {$_.Parameters.virtualMachineName.Value -eq $VM.Name} | Select-Object -First 1).Parameters.publicIpAddressName.Value
-    $NSG = ($NetworkResources | Where-Object {$_.Parameters.virtualMachineName.Value -eq $VM.Name} | Select-Object -First 1).Parameters.networkSecurityGroupId.Value
-    $Image = $VM.StorageProfile.ImageReference.Id
+    $VMs = foreach ($VM in $VMList) {
 
-    [PSCustomObject]@{
+        $NetIp = ($NetworkResources | Where-Object { $_.Parameters.virtualMachineName.Value -eq $VM.Name } | Select-Object -First 1).Parameters.publicIpAddressName.Value
+        $NSG = ($NetworkResources | Where-Object { $_.Parameters.virtualMachineName.Value -eq $VM.Name } | Select-Object -First 1).Parameters.networkSecurityGroupId.Value
+        $Image = $VM.StorageProfile.ImageReference.Id
 
-        VMName        = $VM.Name
-        Region        = $VM.Location
-        VmSize        = $VM.HardwareProfile.VmSize
-        VmHostName    = $VM.OSProfile.ComputerName
-        ResourceGroup = $VM.ResourceGroupName
-        Image         = $(if ($Image) {$Image.Substring($Image.LastIndexOf('/') + 1)})
-        Status        = if ($VM.StatusCode -eq 'OK') {'UP'}
-        DeployState   = $VM.ProvisioningState
-        PublicIP      = ($publicIps | Where-Object{$_.Name -eq $NetIp}).IpAddress
-        NSG           = $(if ($NSG) {$NSG.Substring($NSG.LastIndexOf('/') + 1)})
-    }    
+        [PSCustomObject]@{
+
+            VMName        = $VM.Name
+            Region        = $VM.Location
+            VmSize        = $VM.HardwareProfile.VmSize
+            VmHostName    = $VM.OSProfile.ComputerName
+            ResourceGroup = $VM.ResourceGroupName
+            Image         = $(if ($Image) { $Image.Substring($Image.LastIndexOf('/') + 1) })
+            Status        = if ($VM.StatusCode -eq 'OK') { 'UP' }
+            DeployState   = $VM.ProvisioningState
+            PublicIP      = ($publicIps | Where-Object { $_.Name -eq $NetIp }).IpAddress
+            NSG           = $(if ($NSG) { $NSG.Substring($NSG.LastIndexOf('/') + 1) })
+        }    
+    }
+
+    if ($json) { $VMs | Sort-Object -Descending | ConvertTo-Json -Depth 4 }
+
+    else { $VMs | Sort-Object -Descending | Format-Table } 
+
 }
 
-$VMs | Sort-Object -Descending | Format-Table
+else {
+    Write-Host "Please provide a Resource Group Name" -ForegroundColor Yellow
+}
